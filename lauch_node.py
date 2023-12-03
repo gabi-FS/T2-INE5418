46fbff0f1de79eb71cc0ec73756bb89a6e07bf1e
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2023-12-03 19:29:42 -0300

    Refactor

--- lauch_node.py
+++ lauch_node.py
@@ -2,8 +2,7 @@ import argparse
 import json
 import time
 
-import lib.controller as controller
-import lib.node as node
+from lib.election_node import ElectionNode, NodeAddress
 
 parser = argparse.ArgumentParser(description="Launch a node")
 parser.add_argument("name", type=str, help="The node name")
@@ -15,7 +14,7 @@ JSON_NAME = "port_map.json"
 
 
 def read_file(file_name):
-    with open(file_name, "r") as file:
+    with open(file_name, "r", encoding="utf-8") as file:
         return json.loads(file.read())
 
 
@@ -23,20 +22,21 @@ data = read_file(JSON_NAME)
 this_node_data = data[name]
 server_data = this_node_data["server"]
 client_data = this_node_data["client"]
-this_node = controller.Controller(
-    node.NodeInfo(server_data["host"], server_data["port"]),
-    node.NodeInfo(client_data["host"], client_data["port"]),
-)
 
-neighbors = {}
-for n in this_node_data["neighbors"]:
-    neighbors[int(n["id"])] = node.NodeInfo(n["host"], n["port"])
+server_address = NodeAddress(server_data["host"], server_data["port"])
+client_address = NodeAddress(client_data["host"], client_data["port"])
+neighbors = {int(n["id"]): NodeAddress(n["host"], n["port"]) for n in this_node_data["neighbors"]}
+
+this_node = ElectionNode(this_node_data["id"],
+                         client_address,
+                         server_address,
+                         neighbors)
 
 print(neighbors)
 
-this_node.start(neighbors, this_node_data["id"])
+this_node.start()
 
-time.sleep(30)
+time.sleep(10)
 leader_id = this_node.leader_election()
 
 print("Finalizando o processo, id do líder é", leader_id)
